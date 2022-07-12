8c8bdfea48ca05e8ca90cc67948a0a91da9092d8
Niraj Tiwari <tniraj7@gmail.com>
2022-07-12 23:59:29 +0900

    fix indentation

--- MVCWithUIKIt/Utils/CompositionalGroupAlignment/CompositionalGroupAlignment.swift
+++ MVCWithUIKIt/Utils/CompositionalGroupAlignment/CompositionalGroupAlignment.swift
@@ -14,50 +14,78 @@ enum CompositionalGroupAlignment {
 
 struct CompositionalLayout {
     
-    static func createItem(width: NSCollectionLayoutDimension,
-                           height: NSCollectionLayoutDimension,
-                           spacing: CGFloat
+    static func createItem(
+        width: NSCollectionLayoutDimension,
+        height: NSCollectionLayoutDimension,
+        spacing: CGFloat
     ) -> NSCollectionLayoutItem {
-        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: width,
-                                                                             heightDimension: height))
-        item.contentInsets = NSDirectionalEdgeInsets(top: spacing, leading: spacing, bottom: spacing, trailing: spacing)
+        let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: width,
+                heightDimension: height
+            )
+        )
+        item.contentInsets = NSDirectionalEdgeInsets(
+            top: spacing,
+            leading: spacing,
+            bottom: spacing,
+            trailing: spacing
+        )
         return item
     }
     
-    static func createGroup(alignment: CompositionalGroupAlignment,
-                            width: NSCollectionLayoutDimension,
-                            height: NSCollectionLayoutDimension,
-                            items: [NSCollectionLayoutItem]
+    static func createGroup(
+        alignment: CompositionalGroupAlignment,
+        width: NSCollectionLayoutDimension,
+        height: NSCollectionLayoutDimension,
+        items: [NSCollectionLayoutItem]
     ) -> NSCollectionLayoutGroup {
         switch alignment {
         case .vertical:
-            return NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: width,
-                                                                                       heightDimension: height),
-                                                    subitems: items)
+            return NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: width,
+                    heightDimension: height
+                ),
+                subitems: items
+            )
         case .horizontal:
-            return NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: width,
-                                                                                         heightDimension: height),
-                                                      subitems: items)
+            return NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: width,
+                    heightDimension: height
+                ),
+                subitems: items
+            )
         }
     }
     
-    static func createGroup(alignment: CompositionalGroupAlignment,
-                            width: NSCollectionLayoutDimension,
-                            height: NSCollectionLayoutDimension,
-                            item: NSCollectionLayoutItem,
-                            count: Int
+    static func createGroup(
+        alignment: CompositionalGroupAlignment,
+        width: NSCollectionLayoutDimension,
+        height: NSCollectionLayoutDimension,
+        item: NSCollectionLayoutItem,
+        count: Int
     ) -> NSCollectionLayoutGroup {
         switch alignment {
         case .vertical:
-            return NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: width,
-                                                                                       heightDimension: height),
-                                                    subitem: item,
-                                                    count: count)
+            return NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: width,
+                    heightDimension: height
+                ),
+                subitem: item,
+                count: count
+            )
         case .horizontal:
-            return NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: width,
-                                                                                         heightDimension: height),
-                                                      subitem: item,
-                                                      count: count)
+            return NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(
+                    widthDimension: width,
+                    heightDimension: height
+                ),
+                subitem: item,
+                count: count
+            )
         }
     }
 }
